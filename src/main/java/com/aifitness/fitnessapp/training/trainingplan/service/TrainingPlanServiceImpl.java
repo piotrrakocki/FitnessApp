5060f72898701dbff4e35a23ea0a16194a3709ef
Piotr Rakocki <p.rakocki@gmail.com>
2024-10-11 10:19:08 +0200

    Changed TrainingPlan to TrainingPlanResponse response in TrainingPlanServiceImpl

--- src/main/java/com/aifitness/fitnessapp/training/trainingplan/service/TrainingPlanServiceImpl.java
+++ src/main/java/com/aifitness/fitnessapp/training/trainingplan/service/TrainingPlanServiceImpl.java
@@ -1,32 +1,47 @@
 package com.aifitness.fitnessapp.training.trainingplan.service;
 
+import com.aifitness.fitnessapp.exceptions.UserNotFoundException;
+import com.aifitness.fitnessapp.training.trainingPlanVersion.service.TrainingPlanVersionService;
+import com.aifitness.fitnessapp.training.trainingplan.dto.TrainingPlanResponse;
 import com.aifitness.fitnessapp.training.trainingplan.model.TrainingPlan;
 import com.aifitness.fitnessapp.training.trainingplan.reposiotry.TrainingPlanRepository;
 import com.aifitness.fitnessapp.user.model.AppUser;
 import com.aifitness.fitnessapp.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+
+import lombok.extern.java.Log;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
+@Log
 @Service
 @RequiredArgsConstructor
 public class TrainingPlanServiceImpl implements TrainingPlanService {
 
     private final UserRepository userRepository;
     private final TrainingPlanRepository trainingPlanRepository;
+    private final TrainingPlanVersionService trainingPlanVersionService;
 
     @Override
-    public TrainingPlan createTrainingPlan(Long userId, String planName) {
+    public TrainingPlanResponse createTrainingPlan(Long userId, String planName) {
         AppUser user = userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("Uzytkownik o tym ID nie istnieje"));
-
+                .orElseThrow(() -> new UserNotFoundException("User with this ID not found"));
 
         TrainingPlan trainingPlan = new TrainingPlan();
         trainingPlan.setName(planName);
         trainingPlan.setUser(user);
         trainingPlan.setCreatedAt(LocalDateTime.now());
 
-        return trainingPlanRepository.save(trainingPlan);
+        TrainingPlan saveTrainingPlan = trainingPlanRepository.save(trainingPlan);
+
+        trainingPlanVersionService.createTrainingPlanVersion(trainingPlan.getId());
+
+        return new TrainingPlanResponse(
+                saveTrainingPlan.getId(),
+                saveTrainingPlan.getName(),
+                saveTrainingPlan.getCreatedAt()
+        );
+
     }
 }
