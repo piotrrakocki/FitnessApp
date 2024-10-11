95266e5f390b73a0cf558e887c59c741b0b3f12e
Piotr Rakocki <p.rakocki@gmail.com>
2024-10-11 10:25:09 +0200

    Added createTrainingPlanVersion functionality

---
+++ src/main/java/com/aifitness/fitnessapp/training/trainingPlanVersion/service/TrainingPlanVersionImpl.java
@@ -0,0 +1,31 @@
+package com.aifitness.fitnessapp.training.trainingPlanVersion.service;
+
+import com.aifitness.fitnessapp.exceptions.TrainingPlanNotFoundException;
+import com.aifitness.fitnessapp.training.trainingPlanVersion.model.TrainingPlanVersion;
+import com.aifitness.fitnessapp.training.trainingPlanVersion.repository.TrainingPlanVersionRepository;
+import com.aifitness.fitnessapp.training.trainingplan.model.TrainingPlan;
+import com.aifitness.fitnessapp.training.trainingplan.reposiotry.TrainingPlanRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TrainingPlanVersionImpl implements TrainingPlanVersionService {
+
+    private final TrainingPlanRepository trainingPlanRepository;
+    private final TrainingPlanVersionRepository trainingPlanVersionRepository;
+
+    @Override
+    public TrainingPlanVersion createTrainingPlanVersion(Long trainingPlanId) {
+        TrainingPlan trainingPlan = trainingPlanRepository.findById(trainingPlanId)
+                .orElseThrow(() -> new TrainingPlanNotFoundException("Training plan with this Id does not exist."));
+
+        TrainingPlanVersion newVersion = new TrainingPlanVersion();
+        newVersion.setTrainingPlan(trainingPlan);
+
+        int versionNumber = trainingPlanVersionRepository.countByTrainingPlan(trainingPlan) + 1;
+        newVersion.setVersion(versionNumber);
+
+        return trainingPlanVersionRepository.save(newVersion);
+    }
+}
---
+++ src/main/java/com/aifitness/fitnessapp/training/trainingPlanVersion/service/TrainingPlanVersionService.java
@@ -0,0 +1,8 @@
+package com.aifitness.fitnessapp.training.trainingPlanVersion.service;
+
+import com.aifitness.fitnessapp.training.trainingPlanVersion.model.TrainingPlanVersion;
+
+public interface TrainingPlanVersionService {
+
+    TrainingPlanVersion createTrainingPlanVersion(Long trainingPlanId);
+}
