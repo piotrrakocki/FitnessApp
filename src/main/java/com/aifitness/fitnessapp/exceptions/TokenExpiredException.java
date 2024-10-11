badd915844f74cc5b0b4698b3e496c9b1b630528
Piotr Rakocki <p.rakocki@gmail.com>
2024-10-11 10:13:06 +0200

    Added TokenExpiredException

--- src/main/java/com/aifitness/fitnessapp/exceptions/TokenExpiredException.java
+++ src/main/java/com/aifitness/fitnessapp/exceptions/TokenExpiredException.java
@@ -1,5 +1,9 @@
 package com.aifitness.fitnessapp.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.GONE)
 public class TokenExpiredException extends RuntimeException{
     public TokenExpiredException(String message) {
         super(message);
