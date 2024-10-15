a2fb5f3454959392518ecee89b8da4f0cdaab31d
Piotr Rakocki <p.rakocki@gmail.com>
2024-10-15 10:42:06 +0200

    Deleted @AllArgsConstructor from ExerciseSet

--- src/main/java/com/aifitness/fitnessapp/training/exerciseSet/model/ExerciseSet.java
+++ src/main/java/com/aifitness/fitnessapp/training/exerciseSet/model/ExerciseSet.java
@@ -2,14 +2,12 @@ package com.aifitness.fitnessapp.training.exerciseSet.model;
 
 import com.aifitness.fitnessapp.training.exerciseSession.model.ExerciseSession;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-@AllArgsConstructor
 public class ExerciseSet {
 
     @Id
