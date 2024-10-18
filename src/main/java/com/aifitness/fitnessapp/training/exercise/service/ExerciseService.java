1ca293fdad12e0f85efcaaef43e8ab0a2b26a4f0
Piotr Rakocki <p.rakocki@gmail.com>
2024-10-18 21:06:11 +0200

    Added functionality editExercise

--- src/main/java/com/aifitness/fitnessapp/training/exercise/service/ExerciseService.java
+++ src/main/java/com/aifitness/fitnessapp/training/exercise/service/ExerciseService.java
@@ -6,4 +6,6 @@ import com.aifitness.fitnessapp.training.exercise.dto.ExerciseResponse;
 public interface ExerciseService {
 
     ExerciseResponse addExercise(Long workoutId, ExerciseRequest exerciseRequest);
+
+    ExerciseResponse editExercise(Long exerciseId, String name, int sets, int minReps, int maxReps);
 }
--- src/main/java/com/aifitness/fitnessapp/training/exercise/service/ExerciseServiceImpl.java
+++ src/main/java/com/aifitness/fitnessapp/training/exercise/service/ExerciseServiceImpl.java
@@ -1,14 +1,23 @@
 package com.aifitness.fitnessapp.training.exercise.service;
 
+import com.aifitness.fitnessapp.exceptions.ExerciseNotFoundException;
 import com.aifitness.fitnessapp.exceptions.WorkoutNotFoundException;
 import com.aifitness.fitnessapp.training.exercise.dto.ExerciseRequest;
 import com.aifitness.fitnessapp.training.exercise.dto.ExerciseResponse;
 import com.aifitness.fitnessapp.training.exercise.model.Exercise;
 import com.aifitness.fitnessapp.training.exercise.repository.ExerciseRepository;
+import com.aifitness.fitnessapp.training.trainingSession.service.TrainingSessionService;
+import com.aifitness.fitnessapp.training.trainingplan.trainingPlanVersion.model.TrainingPlanVersion;
+import com.aifitness.fitnessapp.training.trainingplan.trainingPlanVersion.service.TrainingPlanVersionService;
 import com.aifitness.fitnessapp.training.workout.model.Workout;
 import com.aifitness.fitnessapp.training.workout.repository.WorkoutRepository;
+import com.aifitness.fitnessapp.training.workout.service.WorkoutService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +25,9 @@ public class ExerciseServiceImpl implements ExerciseService {
 
     private final ExerciseRepository exerciseRepository;
     private final WorkoutRepository workoutRepository;
+    private final WorkoutService workoutService;
+    private final TrainingSessionService trainingSessionService;
+    private final TrainingPlanVersionService trainingPlanVersionService;
 
     @Override
     public ExerciseResponse addExercise(Long workoutId, ExerciseRequest exerciseRequest) {
@@ -37,6 +49,81 @@ public class ExerciseServiceImpl implements ExerciseService {
                 savedExercise.getSets(),
                 savedExercise.getMinReps(),
                 savedExercise.getMaxReps()
+        );
+    }
+
+    @Override
+    public ExerciseResponse editExercise(Long exerciseId, String name, int sets, int minReps, int maxReps) {
+        Exercise exercise = exerciseRepository.findById(exerciseId)
+                .orElseThrow(() -> new ExerciseNotFoundException("Exercise with exerciseId: " + exerciseId + " not found."));
+
+        if (trainingSessionService.isTrainingSessionExist(exercise.getWorkout().getId())) {
+            TrainingPlanVersion originalVersion = exercise.getWorkout().getTrainingPlanVersion();
+            Long trainingPlanId = originalVersion.getTrainingPlan().getId();
+            TrainingPlanVersion newVersion = trainingPlanVersionService.createTrainingPlanVersion(trainingPlanId);
+
+            for (Workout originalWorkout : originalVersion.getWorkouts()) {
+                Workout clonedWorkout = new Workout();
+                clonedWorkout.setName(originalWorkout.getName());
+                clonedWorkout.setDescription(originalWorkout.getDescription());
+                clonedWorkout.setTrainingPlanVersion(newVersion);
+
+                List<Exercise> clonedExercises = new ArrayList<>();
+                for (Exercise originalExercise : originalWorkout.getExercises()) {
+                    Exercise clonedExercise = new Exercise();
+                    clonedExercise.setName(originalExercise.getName());
+                    clonedExercise.setSets(originalExercise.getSets());
+                    clonedExercise.setMinReps(originalExercise.getMinReps());
+                    clonedExercise.setMaxReps(originalExercise.getMaxReps());
+                    clonedExercise.setWorkout(clonedWorkout);
+
+                    clonedExercises.add(clonedExercise);
+
+                    if (originalExercise.getId().equals(exerciseId)) {
+                        clonedExercise.setName(name);
+                        clonedExercise.setSets(sets);
+                        clonedExercise.setMinReps(minReps);
+                        clonedExercise.setMaxReps(maxReps);
+                    }
+                }
+                clonedWorkout.setExercises(clonedExercises);
+
+                workoutRepository.save(clonedWorkout);
+            }
+
+        } else {
+            boolean hasChange = false;
+
+            if (!Objects.equals(exercise.getName(), name)) {
+                hasChange = true;
+                exercise.setName(name);
+            }
+
+            if (exercise.getSets() != sets) {
+                hasChange = true;
+                exercise.setSets(sets);
+            }
+
+            if (exercise.getMinReps() != minReps) {
+                hasChange = true;
+                exercise.setMinReps(minReps);
+            }
+
+            if (exercise.getMaxReps() != maxReps) {
+                hasChange = true;
+                exercise.setMaxReps(maxReps);
+            }
+
+            if (hasChange) {
+                exerciseRepository.save(exercise);
+            }
+        }
+        return new ExerciseResponse(
+                exerciseId,
+                name,
+                sets,
+                minReps,
+                maxReps
         );
     }
 }
